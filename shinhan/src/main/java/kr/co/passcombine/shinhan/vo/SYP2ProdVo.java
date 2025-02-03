2aa384e9d4600c72d91a3fcc0911c64a30a242fc
jungwon-prj <ghddhfn1858@naver.com>
2025-02-03 15:47:03 +0900

    vo 커밋

---
+++ shinhan/src/main/java/kr/co/passcombine/shinhan/vo/SYP2ProdVo.java
@@ -0,0 +1,164 @@
+package kr.co.passcombine.shinhan.vo;
+
+
+public class SYP2ProdVo {
+	
+	private String p2key;
+	private String dataseq;
+	private String prod_gubun;
+	private String step;
+	private String after_process;
+	private String stack;
+	private String customer_nm;
+	private String prod_div;
+	private String layer;
+	private String model;
+	private String item_no;
+	private String lengh;
+	private String s_pi;
+	private String item_nm;
+	private String item_size_l;
+	private String item_size_r;
+	private String item_size_thin;
+	private String hole;
+	private String x_axis;
+	private String y_axis;
+	private String creator;
+	private String date_created;
+	
+	public String getP2key() {
+		return p2key;
+	}
+	public String getDataseq() {
+		return dataseq;
+	}
+	public String getProd_gubun() {
+		return prod_gubun;
+	}
+	public String getStep() {
+		return step;
+	}
+	public String getAfter_process() {
+		return after_process;
+	}
+	public String getStack() {
+		return stack;
+	}
+	public String getCustomer_nm() {
+		return customer_nm;
+	}
+	public String getProd_div() {
+		return prod_div;
+	}
+	public String getLayer() {
+		return layer;
+	}
+	public String getModel() {
+		return model;
+	}
+	public String getItem_no() {
+		return item_no;
+	}
+	public String getLengh() {
+		return lengh;
+	}
+	public String getS_pi() {
+		return s_pi;
+	}
+	public String getItem_nm() {
+		return item_nm;
+	}
+	public String getItem_size_l() {
+		return item_size_l;
+	}
+	public String getItem_size_r() {
+		return item_size_r;
+	}
+	public String getItem_size_thin() {
+		return item_size_thin;
+	}
+	public String getHole() {
+		return hole;
+	}
+	public String getX_axis() {
+		return x_axis;
+	}
+	public String getY_axis() {
+		return y_axis;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public String getDate_created() {
+		return date_created;
+	}
+	public void setP2key(String p2key) {
+		this.p2key = p2key;
+	}
+	public void setDataseq(String dataseq) {
+		this.dataseq = dataseq;
+	}
+	public void setProd_gubun(String prod_gubun) {
+		this.prod_gubun = prod_gubun;
+	}
+	public void setStep(String step) {
+		this.step = step;
+	}
+	public void setAfter_process(String after_process) {
+		this.after_process = after_process;
+	}
+	public void setStack(String stack) {
+		this.stack = stack;
+	}
+	public void setCustomer_nm(String customer_nm) {
+		this.customer_nm = customer_nm;
+	}
+	public void setProd_div(String prod_div) {
+		this.prod_div = prod_div;
+	}
+	public void setLayer(String layer) {
+		this.layer = layer;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public void setItem_no(String item_no) {
+		this.item_no = item_no;
+	}
+	public void setLengh(String lengh) {
+		this.lengh = lengh;
+	}
+	public void setS_pi(String s_pi) {
+		this.s_pi = s_pi;
+	}
+	public void setItem_nm(String item_nm) {
+		this.item_nm = item_nm;
+	}
+	public void setItem_size_l(String item_size_l) {
+		this.item_size_l = item_size_l;
+	}
+	public void setItem_size_r(String item_size_r) {
+		this.item_size_r = item_size_r;
+	}
+	public void setItem_size_thin(String item_size_thin) {
+		this.item_size_thin = item_size_thin;
+	}
+	public void setHole(String hole) {
+		this.hole = hole;
+	}
+	public void setX_axis(String x_axis) {
+		this.x_axis = x_axis;
+	}
+	public void setY_axis(String y_axis) {
+		this.y_axis = y_axis;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public void setDate_created(String date_created) {
+		this.date_created = date_created;
+	}
+	
+	
+	
+}
